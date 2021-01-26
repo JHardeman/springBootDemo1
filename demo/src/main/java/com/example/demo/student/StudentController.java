12744dbe26b236c047959318868d79179854afda
Jonathan <59838557+JHardeman@users.noreply.github.com>
2021-01-26 14:27:36 -0500

    creatig PostgresSQL database

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,15 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-
 @RequestMapping(path = "api/v1/student")
 // Using the RequestMapping annotation we are creating a specific path where students will be listed
 // that will be locahost8080/api/v1/student
-
 public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
